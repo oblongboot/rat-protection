d00603751fa17a92652cd8735a1c15a0588b9cbb
GitHub Action <action@github.com>
2025-04-26 18:05:38 +0000

    Initialize template repository proper
    Set package name to com.github.oblongboot.ratprotection
    Set project name to rat-protection
    Set modid to ratprotection

--- src/main/java/com/github/oblongboot/ratprotection/ExampleMod.java
+++ src/main/java/com/github/oblongboot/ratprotection/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.oblongboot.ratprotection;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "ratprotection", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
